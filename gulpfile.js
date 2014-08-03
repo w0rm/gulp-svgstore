9d65735fbf9c8d8e27353bc9a2b4f021269660d7
Andrey Kuzmin <unsoundscapes@gmail.com>
2014-08-03 14:55:18 +0400

    Compare screenshots of original svg and image from svgstore

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,57 @@
 var svgstore = require('./index')
 var gulp = require('gulp')
-var svgmin = require('gulp-svgmin')
+var mocha = require('gulp-mocha')
+var connect = require('gulp-connect')
+var inject = require('gulp-inject')
 
-gulp.task('default', function () {
 
-  return gulp.src('test/fixtures/*.svg')
-             .pipe(svgmin())
+gulp.task('svg', function () {
+
+  return gulp.src('test/src/*.svg')
              .pipe(svgstore({ fileName: 'icons.svg'
                             , prefix: 'icon-'
                             , inlineSvg: false
                             , emptyFills: true
                             }))
-             .pipe(gulp.dest('test/'))
+             .pipe(gulp.dest('test/dest/'))
+
+})
+
+
+gulp.task('inline-svg', function () {
+
+  return gulp
+    .src('test/src/inline-svg.html')
+    .pipe(inject(
+      gulp
+        .src('test/src/*.svg')
+        .pipe(svgstore({ fileName: 'icons.svg'
+                       , prefix: 'icon-'
+                       , inlineSvg: true
+                       , emptyFills: true
+                       }))
+    , { transform: function (filePath, file) {
+           return file.contents.toString('utf8')
+         }
+       }
+    ))
+    .pipe(gulp.dest('test/dest/'))
+
+})
+
+
+
+gulp.task('test', ['svg', 'inline-svg'], function () {
+
+  connect.server({
+    port: process.env.PORT || 8888
+  , root: 'test'
+  })
+
+  return gulp
+    .src('test.js', { read: false })
+    .pipe(mocha())
+    .on('end', function () { connect.serverClose() })
+    .on('error', function () { connect.serverClose() })
 
 })
---
+++ test.js
@@ -0,0 +1,83 @@
+/* global describe, it, before, after */
+
+var username = process.env.SAUCE_USERNAME || 'SAUCE_USERNAME'
+var accessKey = process.env.SAUCE_ACCESS_KEY || 'SAUCE_ACCESS_KEY'
+var port = process.env.PORT || 8888
+var wd = require('wd')
+var Q = wd.Q
+var chai = require('chai')
+var fs = require('fs')
+var gm = require('gm')
+var tmp = require('tmp')
+
+tmp.setGracefulCleanup()
+chai.should()
+
+
+function writeScreenshot (data) {
+  return Q.Promise(function (resolve, reject) {
+    tmp.tmpName(function (err, path) {
+      if (err) reject(new Error(err))
+      fs.writeFile(path, data, 'base64', function (err) {
+        if (err) reject(new Error(err))
+        resolve(path)
+      })
+    })
+  })
+}
+
+
+function compareScreenshots (path1, path2) {
+  return Q.Promise(function (resolve, reject) {
+    gm.compare(path1, path2, function (err, isEqual, equality, raw) {
+      if (err) reject(new Error(err))
+      resolve(isEqual, equality, raw)
+    })
+  })
+}
+
+
+describe('gulp-svgstore usage test', function() {
+
+  this.timeout(10000)
+
+  var browser
+
+  before(function (done) {
+    browser = wd.promiseChainRemote('ondemand.saucelabs.com', 80, username, accessKey)
+    browser.init({ browserName: 'chrome' }).nodeify(done)
+  })
+
+  after(function (done) {
+    browser.quit().nodeify(done)
+  })
+
+  it('stored image should equal original svg', function (done) {
+    var screenshot1, screenshot2
+    browser
+      .get('http://localhost:' + port + '/inline-svg.html')
+      .title()
+      .then(function (title) {
+        title.should.equal('gulp-svgstore')
+      })
+      .takeScreenshot()
+      .then(writeScreenshot)
+      .then(function (path) {
+        screenshot1 = path
+      })
+      .get('http://localhost:' + port + '/dest/inline-svg.html')
+      .takeScreenshot()
+      .then(writeScreenshot)
+      .then(function (path) {
+        screenshot2 = path
+      })
+      .then(function () {
+        return compareScreenshots(screenshot1, screenshot2)
+      })
+      .then(function (isEqual, equality, raw) { // jshint ignore:line
+        isEqual.should.be.true
+      })
+      .nodeify(done)
+  })
+
+})
