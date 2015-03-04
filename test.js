fdc1584c8e943c33388a7a85ce6e730795466efc
Pierre-Alexandre St-Jean <pa@stjean.me>
2015-03-03 20:43:14 -0500

    Fix #31: Fix tests for new defs format, add test for to parent def

--- test.js
+++ test.js
@@ -119,6 +119,7 @@ describe('gulp-svgstore unit test', function () {
       var result = file.contents.toString()
       var target =
       '<svg xmlns="http://www.w3.org/2000/svg">' +
+      '<defs/>' +
       '<symbol id="circle" viewBox="0 0 4 4"><circle cx="2" cy="2" r="1"/></symbol>' +
       '<symbol id="square"><rect x="1" y="1" width="2" height="2"/></symbol>' +
       '</svg>'
@@ -154,6 +155,7 @@ describe('gulp-svgstore unit test', function () {
       var result = file.contents.toString()
       var target =
       '<svg xmlns="http://www.w3.org/2000/svg">' +
+      '<defs/>' +
       '<symbol id="square"><circle cx="2" cy="2" r="1"/></symbol>' +
       '</svg>'
       assert.equal( result, target )
@@ -184,6 +186,33 @@ describe('gulp-svgstore unit test', function () {
 
   })
 
+  it('should merge defs to parent svg file', function(done){
+
+    var stream = svgstore({ inlineSvg: true })
+
+    stream.on('data', function(file){
+      var result = file.contents.toString()
+      console.log(result)
+      var target =
+        '<svg xmlns="http://www.w3.org/2000/svg">' +
+        '<defs><circle id="circ" cx="2" cy="2" r="1"/></defs>' +
+        '<symbol id="circle" viewBox="0 0 4 4"/>' +
+        '</svg>'  
+      assert.equal( result, target )
+      done()
+
+    })
+
+    stream.write(new gutil.File({
+      contents: new Buffer('<svg viewBox="0 0 4 4"><defs><circle id="circ" cx="2" cy="2" r="1"/></svg>/defs><circle cx="2" cy="2" r="1"/></svg>')
+    , path: 'circle.svg'
+    }))
+
+    stream.end()
+
+
+  })
+  
   it('should emit error if files have the same name', function (done) {
 
       var stream = svgstore()
