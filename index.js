75e3f275ef43c60e2ca24e6ee89f816e67245859
Andrey Kuzmin <unsoundscapes@gmail.com>
2015-06-25 21:35:35 +0200

    Use Stream.Transform directly without wrapper

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cheerio = require('cheerio')
 var path = require('path')
-var through2 = require('through2')
 var gutil = require('gulp-util')
+var Stream = require('stream')
 
 module.exports = function (config) {
 
@@ -12,7 +12,7 @@ module.exports = function (config) {
   var inlineSvg = config.inlineSvg || false
   var ids = {}
 
-  var resultSvg = '<svg xmlns="http://www.w3.org/2000/svg" ><defs/></svg>'
+  var resultSvg = '<svg xmlns="http://www.w3.org/2000/svg"><defs/></svg>'
   if (!inlineSvg) {
     resultSvg =
       '<?xml version="1.0" encoding="UTF-8"?>' +
@@ -24,68 +24,68 @@ module.exports = function (config) {
   var $ = cheerio.load(resultSvg, { xmlMode: true })
   var $combinedSvg = $('svg')
   var $combinedDefs = $('defs')
+  var stream = new Stream.Transform({ objectMode: true })
 
-  return through2.obj(
+  stream._transform = function transform (file, encoding, cb) {
 
-    function transform (file, encoding, cb) {
-
-      if (file.isStream()) {
-        return cb(new gutil.PluginError('gulp-svgstore', 'Streams are not supported!'))
-      }
-
-      if (!file.cheerio) {
-        file.cheerio = cheerio.load(file.contents.toString(), { xmlMode: true })
-      }
-
-      var $svg = file.cheerio('svg')
-      var idAttr = path.basename(file.relative, path.extname(file.relative))
-      var viewBoxAttr = $svg.attr('viewBox')
-      var $symbol = $('<symbol/>')
-
-      if (idAttr in ids) {
-        return cb(new gutil.PluginError('gulp-svgstore', 'File name should be unique: ' + idAttr))
-      }
-
-      ids[idAttr] = true
-
-      if (!fileName) {
-        fileName = path.basename(file.base)
-        if (fileName === '.' || !fileName) {
-          fileName = 'svgstore.svg'
-        } else {
-          fileName = fileName.split(path.sep).shift() + '.svg'
-        }
-      }
-
-      if (file && isEmpty) {
-        isEmpty = false
-      }
-
-      $symbol.attr('id', idAttr)
-      if (viewBoxAttr) {
-        $symbol.attr('viewBox', viewBoxAttr)
-      }
-
-      var $defs = file.cheerio('defs')
-      if ($defs.length > 0) {
-        $combinedDefs.append($defs.contents())
-        $defs.remove()
-      }
-
-      $symbol.append($svg.contents())
-      $combinedSvg.append($symbol)
-      cb()
+    if (file.isStream()) {
+      return cb(new gutil.PluginError('gulp-svgstore', 'Streams are not supported!'))
     }
 
-  , function flush (cb) {
-      if (isEmpty) return cb()
-      if ($combinedDefs.contents().length === 0) {
-        $combinedDefs.remove()
-      }
-      var file = new gutil.File({ path: fileName, contents: new Buffer($.xml()) })
-      file.cheerio = $
-      this.push(file)
-      cb()
+    if (!file.cheerio) {
+      file.cheerio = cheerio.load(file.contents.toString(), { xmlMode: true })
     }
-  )
+
+    var $svg = file.cheerio('svg')
+    var idAttr = path.basename(file.relative, path.extname(file.relative))
+    var viewBoxAttr = $svg.attr('viewBox')
+    var $symbol = $('<symbol/>')
+
+    if (idAttr in ids) {
+      return cb(new gutil.PluginError('gulp-svgstore', 'File name should be unique: ' + idAttr))
+    }
+
+    ids[idAttr] = true
+
+    if (!fileName) {
+      fileName = path.basename(file.base)
+      if (fileName === '.' || !fileName) {
+        fileName = 'svgstore.svg'
+      } else {
+        fileName = fileName.split(path.sep).shift() + '.svg'
+      }
+    }
+
+    if (file && isEmpty) {
+      isEmpty = false
+    }
+
+    $symbol.attr('id', idAttr)
+    if (viewBoxAttr) {
+      $symbol.attr('viewBox', viewBoxAttr)
+    }
+
+    var $defs = file.cheerio('defs')
+    if ($defs.length > 0) {
+      $combinedDefs.append($defs.contents())
+      $defs.remove()
+    }
+
+    $symbol.append($svg.contents())
+    $combinedSvg.append($symbol)
+    cb()
+  }
+
+  stream._flush = function flush (cb) {
+    if (isEmpty) return cb()
+    if ($combinedDefs.contents().length === 0) {
+      $combinedDefs.remove()
+    }
+    var file = new gutil.File({ path: fileName, contents: new Buffer($.xml()) })
+    file.cheerio = $
+    this.push(file)
+    cb()
+  }
+
+  return stream;
 }
